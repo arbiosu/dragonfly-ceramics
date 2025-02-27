c64068332af077b9b48bb7219205936278d8463d
Arberim Ame <amea@oregonstate.edu>
2025-02-26 19:07:38 -0500

    Componentize shop link

---
+++ project/src/components/shop-link.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+
+export default function ShopLink() {
+    return (
+        <Link 
+            href={"#"}
+            className="inline-flex items-center justify-center px-5 py-3 mr-3
+            text-base font-bold text-center text-white rounded-lg bg-stone-800
+            hover:bg-blue-700 focus:ring-4 focus:ring-white"
+        >
+            Shop now
+            <svg 
+                className="w-5 h-5 ml-2 -mr-1"
+                fill="currentColor" 
+                viewBox="0 0 20 20" 
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path 
+                    fillRule="evenodd" 
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clipRule="evenodd">
+                </path>
+            </svg>
+        </Link>
+    )
+}
