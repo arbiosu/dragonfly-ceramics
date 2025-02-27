207e3a698eabeec52133326a2d05945a491f274c
Arberim Ame <amea@oregonstate.edu>
2025-02-27 15:54:39 -0500

    Fix icon not centered on mobile

--- project/src/components/subscribe-card.tsx
+++ project/src/components/subscribe-card.tsx
@@ -34,7 +34,7 @@ export default function SubscribeCard() {
         // TODO: hit api
     }
         return (
-            <div className="max-w-md w-full mx-auto rounded-lg shadow-lg bg-white p-6 flex flex-col h-full">
+            <div className="w-full mx-auto rounded-lg shadow-lg bg-white p-6 flex flex-col h-full">
                 <div className="text-center mb-auto justify-items-center space-y-8">
                     <h2 className="text-xl font-bold text-df-text mb-2">
                         Subcribe
