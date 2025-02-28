ee615f89d1349c510a05eb4e17ed07aa64ae1a22
Arberim Ame <amea@oregonstate.edu>
2025-02-27 23:42:32 -0500

    Update pages

--- project/src/app/about/page.tsx
+++ project/src/app/about/page.tsx
@@ -2,9 +2,10 @@ import Image from "next/image";
 import ShopLink from "@/components/shop-link";
 import ImageGrid from "@/components/image-grid";
 import SubscribeCard from "@/components/subscribe-card";
-import FAQCarousel from "@/components/faqs";
+import { FAQHorizontalScroll} from "@/components/faqs";
 import { SocialMediaLinksCard } from "@/components/socialmedia";
 import potterySvg from "@/../public/ceramic-vase.svg"
+import logo from "@/../public/logo-df-text.png"
 
 
 export default function About() {
@@ -15,17 +16,42 @@ export default function About() {
         { heading: "Sustainable", src: "/pottery-hand.jpeg"}, 
     ]
 
-    const sampleFaqs = [
-        { id: 1, question: "Blah blah blah?", answer: "Yes!"},
-        { id: 2, question: "Blha blah blha?", answer: "No!"},
-        { id: 3, question: "Blha blah blha?", answer: "Yes!"},
-        { id: 4, question: "Blha blah blha?", answer: "No!"},
-        { id: 5, question: "Blha blah blha?", answer: "Yes!"},
-    ]
+    const faqs = [
+        {
+          id: 1,
+          question: "What if my purchase is damaged or delayed?",
+          answer: "All products are packaged with love and care and I ship them out ASAP with a tracking number. However, if your piece comes damaged, please email me photos within 3 days of arrival. I will try my best to replace the item with something similar or a full or partial refund can be given."
+        },
+        {
+          id: 2,
+          question: "Where do you ship?",
+          answer: "As of now, I only ship in the United States but hope to eventually expand internationally!"
+        },
+        {
+          id: 3,
+          question: "How do I take care of my Pottery?",
+          answer: "All of my pieces are dishwasher and microwave safe! However, I always recommend handwashing. Pieces that are not dishwasher and microwave safe will be labeled in the description of the piece."
+        },
+        {
+          id: 4,
+          question: "Do you accept returns?",
+          answer: "Please make your purchases carefully as all sales are final. I only accept returns if the wrong item was shipped to you."
+        },
+        {
+          id: 5,
+          question: "When do you restock?",
+          answer: "I have no particular schedule for restocks. As long as you are following my socials and/or signed up for my newsletter, you will be alerted when a drop happens!"
+        },
+        {
+          id: 6,
+          question: "How do I purchase a custom or wholesale order?",
+          answer: "Please fill out the form under the “contact” page and allow up to 72 hours for me to respond!"
+        }
+      ];
 
     return (
         <>
-        <section className="bg-df-bg mx-auto px-4 py-12 md:py20">
+        <section className="bg-df-bg mx-auto px-4 py-20 md:py20">
             <div className="grid md:grid-cols-2 gap-12 items-center">
                 <div className="relative aspect-square bg-df-bg">
                     <Image
@@ -54,14 +80,14 @@ export default function About() {
         <section className="p-8 grid md:grid-cols-3 gap-2 bg-df-bg">
             <SubscribeCard />
             <Image
-                src="/logo-black.png"
+                src={logo}
                 alt="Dragonfly Ceramics Logo"
-                width={600}
-                height={400}
             />
             <SocialMediaLinksCard />
         </section>
-        <FAQCarousel faqs={sampleFaqs} />
+        <section className="pt-20">
+            <FAQHorizontalScroll faqs={faqs} />
+        </section>
         <ImageGrid items={imageGridItems} />
         </>
     )
