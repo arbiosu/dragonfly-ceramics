acae53c453c57daa2b819e608c9e42f8e02fd392
Arberim Ame <amea@oregonstate.edu>
2025-02-27 23:40:51 -0500

    Add supabase url for content hosting

--- project/next.config.ts
+++ project/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        port: ''
+      }
+    ]
+  }
 };
 
 export default nextConfig;
