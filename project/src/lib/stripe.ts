65ccc1d2701304c3053971d3c1351a9c82c5227a
Arberim Ame <amea@oregonstate.edu>
2025-02-28 23:35:14 -0500

    Update types

--- project/src/lib/stripe.ts
+++ project/src/lib/stripe.ts
@@ -4,9 +4,9 @@ import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
-export async function getProducts() {
-    const products = await stripe.products.list()
-    console.log(products)
-    return products
+export async function getProducts(): Promise<Stripe.Product[]> {
+    const products = await stripe.products.list({ expand: ["data.default_price"]})
+    const data: Stripe.Product[] = products.data
+    return data
 }
 
