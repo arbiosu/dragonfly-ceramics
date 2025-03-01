d882ffa0a7f1f0789f75df419d90b0dffe12f9b2
Arberim Ame <amea@oregonstate.edu>
2025-02-28 23:34:50 -0500

    Update shop page, create layout for shop and cart

---
+++ project/src/app/shop/layout.tsx
@@ -0,0 +1,13 @@
+import { CartProvider } from "@/contexts/CartContext";
+
+export default function ShopLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <CartProvider>
+            {children}
+        </CartProvider>
+    );
+};
--- project/src/app/shop/page.tsx
+++ project/src/app/shop/page.tsx
@@ -1,16 +1,13 @@
 import { getProducts } from "@/lib/stripe"
+import ProductGrid from "@/components/product-grid"
 
 
 export default async function Shop() {
-    const products = getProducts()
+    const products = await getProducts()
     console.log(products)
     return (
-        <div className="container mx-auto">
-            <ul>
-                <li>
-                    Produts
-                </li>
-            </ul>
+        <div className="container mx-auto py-20">
+            <ProductGrid products={products} />
         </div>
     )
 }
