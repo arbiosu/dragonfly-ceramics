19fbbf2ca5a85d4cb5368d2e8ea89db42b7f0e90
Arberim Ame <amea@oregonstate.edu>
2025-03-31 22:35:04 -0400

    Update bodySizeLimit

--- project/next.config.ts
+++ project/next.config.ts
@@ -19,6 +19,14 @@ const nextConfig: NextConfig = {
     minimumCacheTTL: 2678400, // 31 days
     deviceSizes: [640, 750, 828, 1080, 1200], // Limit size variations
     imageSizes: [16, 32, 64, 96], // Limit size variations
+  },
+  api: {
+    bodyParser: {
+        sizeLimit: '2mb',
+    },
+  },
+  serverActions: {
+    bodySizeLimit: '2mb',
   }
 };
 
