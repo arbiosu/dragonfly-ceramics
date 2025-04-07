7d6671eb9a2d35ade2b2762bcfa6b8e99c5e2524
Arberim Ame <amea@oregonstate.edu>
2025-04-07 13:19:16 -0400

    Revert to old admin portal

--- project/src/components/admin/portal.tsx
+++ project/src/components/admin/portal.tsx
@@ -1,36 +1,15 @@
-'use server';
+import AdminPortal from "@/components/admin/portal";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
 
-import {
-  fetchProducts,
-  serializeStripeProduct,
-  type Product,
-  updateProductImagesById,
-} from '@/lib/stripe/utils';
 
-export default async function AdminPortal() {
-  const products = await fetchProducts();
-  const serializedProducts: Product[] = [];
-  for (const product of products) {
-    const serialized = await serializeStripeProduct(product);
-    serializedProducts.push(serialized);
-  }
-  const fix = async () => {
-    for (const p of serializedProducts) {
-      for (const img of p.images) {
-        if (img[8] === 'f') {
-          console.log('Stripe link, do not touch!!');
-          continue;
-        }
-        console.log('Before:\n', img, '\n');
-        const test = img + '-320w.webp';
-        console.log('After:\n', test, '\n');
-      }
+export default async function Portal() {
+    const supabase = await createClient();
+    const { data, error } = await supabase.auth.getUser();
+    if (error || !data?.user) {
+        redirect('/')
     }
-  };
-  fix();
-  return (
-    <section className='py-20'>
-      <h1 className='text-center text-2xl text-df-text'>Admin Portal</h1>
-    </section>
-  );
+    return (
+        <AdminPortal />
+    );
 }
