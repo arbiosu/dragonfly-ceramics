73633d20a9b5611b24152ccb36adb31a8d8b76c3
Arberim Ame <amea@oregonstate.edu>
2025-04-14 17:55:21 -0400

    Update admin components

--- project/src/components/admin/admin-product-card.tsx
+++ project/src/components/admin/admin-product-card.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import Image from 'next/image';
-import { Stripe } from 'stripe';
+import Link from 'next/link';
 import { useState, useRef } from 'react';
+import { Tables } from '@/lib/supabase/database';
 import { uploadImage } from '@/lib/supabase/model';
 import { updateProductImagesById } from '@/lib/stripe/utils';
 
-export default function AdminProductCard(props: { product: Stripe.Product }) {
+export default function AdminProductCard(props: {
+  product: Tables<'products'>;
+}) {
   const [file, setFile] = useState<File | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const fileInputRef = useRef<HTMLInputElement>(null);
@@ -26,13 +28,13 @@ export default function AdminProductCard(props: { product: Stripe.Product }) {
       return;
     }
 
-    if (file?.size / 1024 / 1024 > 2) {
-      alert('File size is too large. Image must be less than 2MB');
+    if (file?.size / 1024 / 1024 >= 1) {
+      alert('File size is too large. Image must be less than 1MB');
       return;
     }
 
     try {
-      const imgPath = await uploadImage(file, product.id);
+      const imgPath = await uploadImage(file, product.stripe_id);
       if (!imgPath) {
         alert('File upload failed. Please try again later.');
         return;
@@ -41,7 +43,7 @@ export default function AdminProductCard(props: { product: Stripe.Product }) {
       newImages.push(
         `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/content/${imgPath}`
       );
-      await updateProductImagesById(product.id, newImages);
+      await updateProductImagesById(product.stripe_id, newImages);
     } catch (error) {
       alert('Upload failed. Please try again.');
       console.log(error);
@@ -54,58 +56,79 @@ export default function AdminProductCard(props: { product: Stripe.Product }) {
   const { product } = props;
 
   return (
-    <div className='shadow-t-md group relative w-full max-w-sm overflow-hidden rounded-lg bg-df-bg'>
-      {/* Image Container */}
-      <div className='relative aspect-square w-full'>
-        <Image
-          src={product.images[0]}
-          alt={product.description || 'No Description'}
-          className='object-cover'
-          fill
-          sizes='(max-width: 768px) 100vw, 256px'
-        />
-      </div>
+    <div className='relative w-full max-w-sm overflow-hidden bg-df-bg'>
       <div className='p-2'>
-        <h3 className='truncate text-lg font-semibold text-df-text'>
-          {product.name}
-        </h3>
+        <h3 className='text-lg font-semibold text-df-text'>{product.name}</h3>
         <p className='mb-4 flex-grow text-sm text-df-text'>
-          {product.description}
+          Created at: {new Date(product.created_at).toLocaleDateString()}
         </p>
-        <div className='mt-auto'>
-          {product.default_price &&
-          typeof product.default_price !== 'string' &&
-          product.default_price.unit_amount ? (
-            <p className='mb-3 text-xl font-bold text-df-text'>
-              ${product.default_price?.unit_amount / 100}
-            </p>
-          ) : (
-            <p className='mb-3 text-xl font-bold text-df-text'>
-              No pricing data
-            </p>
-          )}
+        <p className='mb-4 flex-grow text-sm text-df-text'>
+          Is active: {product.active ? 'TRUE' : 'FALSE'}
+        </p>
+        <p className='mb-4 flex-grow text-sm text-df-text'>
+          Type: {product.type}
+        </p>
+        <p className='mb-4 flex-grow text-sm text-df-text'>
+          Inventory: {product.inventory}
+        </p>
+        <p className='mb-4 flex-grow text-sm text-df-text'>
+          Weight: {product.weight}
+        </p>
+        <p className='mb-4 flex-grow text-sm text-df-text'>
+          Length: {product.length}
+        </p>
+        <p className='mb-4 flex-grow text-sm text-df-text'>
+          Width: {product.width}
+        </p>
+        <p className='mb-4 flex-grow text-sm text-df-text'>
+          Height: {product.height}
+        </p>
+        <p className='mb-4 flex-grow text-sm text-df-text'>
+          Price: {product.price / 100}
+        </p>
+        <div className='mb-4'>
+          <p className='mb-2 text-sm text-df-text'>Images: </p>
+          <div className='h-32 overflow-y-auto rounded border border-gray-200 bg-white p-2'>
+            {product.images.length > 0 ? (
+              product.images.map((url, index) => (
+                <div key={index} className='mb-1 truncate text-sm'>
+                  <Link
+                    href={url}
+                    prefetch={false}
+                    className='text-df-text hover:underline'
+                  >
+                    Image #{index + 1}: {url}
+                  </Link>
+                </div>
+              ))
+            ) : (
+              <p className='text-sm italic text-gray-500'>
+                No images available
+              </p>
+            )}
+          </div>
         </div>
-        <form onSubmit={handleSubmit}>
-          <label className='text-df-text'>
-            {`Add an Image to ${product.name}`}
-          </label>
-          <input
-            type='file'
-            accept='image/*'
-            onChange={handleFileChange}
-            ref={fileInputRef}
-            className='text-df-text'
-            required
-          />
-          <button
-            type='submit'
-            disabled={loading}
-            className='w-full rounded bg-df-text p-2 text-white'
-          >
-            {loading ? 'Uploading...' : 'Upload Image'}
-          </button>
-        </form>
       </div>
+      <form onSubmit={handleSubmit}>
+        <label className='mb-4 text-df-text'>
+          {`Add an Image to ${product.name}`}
+        </label>
+        <input
+          type='file'
+          accept='image/*'
+          onChange={handleFileChange}
+          ref={fileInputRef}
+          className='mb-4 text-df-text'
+          required
+        />
+        <button
+          type='submit'
+          disabled={loading}
+          className='w-full rounded bg-df-text p-2 text-white'
+        >
+          {loading ? 'Uploading...' : 'Upload Image'}
+        </button>
+      </form>
     </div>
   );
 }
