0f73cf411414f9d36843258bb775ca99ef28aef4
Arberim Ame <amea@oregonstate.edu>
2025-07-09 22:32:45 -0400

    changes

--- project/src/components/shop/shop-button.tsx
+++ project/src/components/shop/shop-button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center hover:text-white text-black bg-white hover:bg-white/10 justify-center rounded-full text-xl transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center hover:text-white text-black bg-white hover:bg-white/10 justify-center rounded-3xl text-xl transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
